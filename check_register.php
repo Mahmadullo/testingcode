46ae79bdb1994c59550b32a419b928bbf7f36c43
Mahmadullo <abdulloev156380@gmail.com>
2023-09-05 03:24:32 +0800

    Fixing, new version code

--- check_register.php
+++ check_register.php
@@ -1,35 +1,42 @@
 <?php
-	session_start(); // Start the session
+	session_start(); // Начинаем сессию
 	
 	if (isset($_POST['submit'])) {
 		$email = $_POST['userEmail'];
-		$password = $_POST['userEmail'];
-		$emailToCheck = $email;
+		$password = $_POST['userPassword']; // Исправлено на 'userPassword'
 		
+		// Создаем соединение с базой данных (замените на ваши реальные данные)
 		$conn = new PDO('mysql:host=localhost; dbname=db_user;', 'root', '');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
-		$stm = $conn->prepare("SELECT * FROM `users` WHERE email = :email");
-		$stm->bindParam(':email', $emailToCheck, PDO::PARAM_STR);
-		$stm->execute();
-		$user = $stm->fetchAll(PDO::FETCH_ASSOC);
+		// Подготавливаем запрос для выборки пользователя по email
+		$stmt = $conn->prepare("SELECT * FROM `users` WHERE email = :email");
+		$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+		$stmt->execute();
+		$user = $stmt->fetch(PDO::FETCH_ASSOC);
 		
 		if ($user) {
-			$_SESSION['flash_message'] = "Email already exists. Please log in or use a different email.";
-			$_SESSION['alert'] = 'alert-danger';
-			
-			// Redirect to registration page
+			// Пользователь с указанным email найден
+			if (password_verify($password, $user['password'])) {
+				// Пароли совпадают, выполните действия аутентификации
+				$_SESSION['user_id'] = $user['id'];
+				$_SESSION['flash_message'] = 'Аутентификация успешна.';
+				$_SESSION['alert'] = 'alert-success';
+				// Перенаправление на защищенную страницу после аутентификации
+				header("Location: secure_page.php");
+			} else {
+				// Неверный пароль
+				$_SESSION['flash_message'] = 'Неверный пароль';
+				$_SESSION['alert'] = 'alert-danger';
+				// Перенаправление на страницу входа с сообщением об ошибке
+				header("Location: page_login.php");
+			}
 		} else {
-			// Email doesn't exist, proceed with registration
-			$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-			$stmt = $conn->prepare("INSERT INTO `users` (email, password) VALUES (:email, :password)");
-			$stmt->bindParam(':email', $email, PDO::PARAM_STR);
-			$stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
-			$stmt->execute();
-			$_SESSION['flash_message'] = "Registration is successful!";
-			$_SESSION['alert'] = 'alert-success';
-			// Redirect to registration page
-			
+			// Пользователь с указанным email не найден
+			$_SESSION['flash_message'] = 'Пользователь с указанным email не найден';
+			$_SESSION['alert'] = 'alert-danger';
+			// Перенаправление на страницу входа с сообщением об ошибке
+			header("Location: page_login.php");
 		}
-		header("Location: page_register.php");
+		exit();
 	}
--- login.php
+++ login.php
@@ -17,53 +17,13 @@
 		$userData = getUserDataByEmail($dbConnection, $inputEmailUser);
 
 //
-//		if ($userData && password_verify($inputPasswordUser, $userData['password'])) {
-//			// Успешная аутентификация, перенаправляем на страницу пользователей
-//			header('Location: users.php');
-//		} else {
-//			// Неверные учетные данные, устанавливаем сообщение об ошибке и перенаправляем на страницу входа
-//			$_SESSION['flash_message'] = 'Неверный email или пароль';
-//			$_SESSION['alert'] = 'danger';
-//			header('Location: page_login.php');
-//		}
-//	}
-//
-
-//		if ($userData) {
-//			// Проверяем хэшированный пароль
-//			if (password_verify($inputPasswordUser, $userData['password'])) {
-//				// Успешная аутентификация, перенаправляем на страницу пользователей
-//				header('Location: users.php');
-//			} else {
-//				// Неверный пароль
-//				echo "Неверный пароль";
-//			}
-//		} else {
-//			// Пользователь с указанным email не найден
-//			echo "Пользователь с указанным email не найден";
-//		}
-//
-//		exit();
-//	}
-		
-		if ($userData) {
-			// Отладочная информация
-			var_dump($inputPasswordUser);
-			var_dump($userData['password']);
-			
-			// Проверяем хэшированный пароль
-			if (password_verify($inputPasswordUser, $userData['password'])) {
-				// Успешная аутентификация, перенаправляем на страницу пользователей
-				header('Location: users.php');
-			} else {
-				// Неверный пароль
-				echo "Неверный пароль";
-			}
+		if ($userData && password_verify($inputPasswordUser, $userData['password'])) {
+			// Успешная аутентификация, перенаправляем на страницу пользователей
+			header('Location: users.php');
 		} else {
-			// Пользователь с указанным email не найден
-			echo "Пользователь с указанным email не найден";
+			// Неверные учетные данные, устанавливаем сообщение об ошибке и перенаправляем на страницу входа
+			$_SESSION['flash_message'] = 'Неверный email или пароль';
+			$_SESSION['alert'] = 'danger';
+			header('Location: page_login.php');
 		}
-		
-		exit();
-		var_dump($userData);
 	}
