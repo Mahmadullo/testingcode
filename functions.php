65a2ba088cac47348b3fae20b9553d4bed511e6d
Mahmadullo <abdulloev156380@gmail.com>
2023-09-07 15:50:48 +0800

    Исправки, по коду в айдишники

--- functions.php
+++ functions.php
@@ -28,23 +28,9 @@
 	
 	function addUser($conn, $table, $user)
 	{
-		// Prepare the SQL statement
-		$stmt = $conn->prepare("INSERT INTO `$table`(`username`, `job_title`, `status`, `image`, `phone`, `address`, `email`, `vk`, `telegram`, `instagram`) VALUES (:username, :job_title, :status, :image, :phone, :address, :email, :vk, :telegram, :instagram)");
-		
-		// Bind parameters
-		$stmt->bindParam(':username', $user['username']);
-		$stmt->bindParam(':job_title', $user['job_title']);
-		$stmt->bindParam(':status', $user['status']);
-		$stmt->bindParam(':image', $user['image']);
-		$stmt->bindParam(':phone', $user['phone']);
-		$stmt->bindParam(':address', $user['address']);
-		$stmt->bindParam(':email', $user['email']);
-		$stmt->bindParam(':vk', $user['vk']);
-		$stmt->bindParam(':telegram', $user['telegram']);
-		$stmt->bindParam(':instagram', $user['instagram']);
-		
-		// Execute the SQL statement
-		return $stmt->execute();
+		$stmt = $conn->prepare("INSERT INTO `users`(`email`) VALUES ('ss')");
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 	
 	function isUser($role): bool
@@ -54,15 +40,16 @@
 	
 	function isAdmin($user)
 	{
-		if (is_logged_in() && isset($user['role']) && $user['role'] === 'admin') {
-			return true;
+		if (is_logged_in()) {
+			if ($user['role'] === 'admin')
+				return;
 		}
 		return false;
 	}
 	
 	function is_logged_in()
 	{
-		if (isset($_SESSION['user_id'])) {
+		if (isset($_SESSION['username'])) {
 			return true;
 		}
 		return false;
@@ -75,30 +62,8 @@
 	
 	function get_authenticatedUser()
 	{
-		if (is_logged_in() && isset($_SESSION['user_data'])) {
-			return $_SESSION['user_data'];
+		if (is_logged_in()) {
+			return $_SESSION['username'];
 		}
 		return false;
-	}
-	
-	function compareUserIds($userId, $idfromSession)
-	{
-		// Проверка, что идентификатор из сессии и из базы данных существуют и совпадают
-		if ($userId['id'] == $idfromSession['id']) {
-			return true;
-		}
-		return false;
-	}
-	
-	//После много попытки начал по видео решение кода но без результатно
-	function login($email, $password)
-	{
-		$user =
-			[
-				'id' => '1',
-				'email' => 'mahmadullo.1111@gmail.com',
-				'role' => 'admin'
-			];
-		
-		$_SESSION['user'] = $user;
 	}
